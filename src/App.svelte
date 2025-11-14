363c2593d673ce580cde0fa27dcdfe3e8f7e02fd
Iwansal64 <iwansal64@gmail.com>
2025-11-14 09:07:24 +0700

    [new] First Initialization

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+      import Router from "svelte-spa-router";
+      import Home from "./routes/Home.svelte";
+      import Gate from "./routes/Gate.svelte";
+      import Vote from "./routes/Vote.svelte";
+
+      const routes = {
+            "/": Home,
+            "/gate": Gate,
+            "/vote": Vote
+      };
+</script>
+
+<svelte:head>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+      <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&family=Saira:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</svelte:head>
+
+
+<Router {routes} />
