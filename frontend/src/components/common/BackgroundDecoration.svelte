70461d422939b86ee91a2b74bfebe0bbf0180082
Iwansal64 <iwansal64@gmail.com>
2026-01-08 16:31:21 +0700

    [new] Change Background for Admin

--- frontend/src/components/common/BackgroundDecoration.svelte
+++ frontend/src/components/common/BackgroundDecoration.svelte
@@ -16,4 +16,6 @@
         <img class="absolute h-full -left-100 -z-10 scale-x-90" src={Pillar} alt="">
         <img class="absolute h-full -right-100 -z-10 scale-x-90" src={Pillar} alt="">
         <img class="absolute w-full bottom-0 left-1/2 -translate-x-1/2 -z-10 opacity-50" src={Stage} alt="">
+        <div class="absolute w-[200%] aspect-square bottom-0 left-0 -translate-x-1/2 translate-y-1/2 bg-radial from-yellow-400 to-transparent from-10% to-40% opacity-50 -z-100"></div>
+        <div class="absolute w-[80%] aspect-square -bottom-full left-1/2 -translate-x-1/2 bg-radial from-white to-transparent from-0% to-70% opacity-50 -z-100"></div>
 </div>
--- frontend/src/routes/AdminGate.svelte
+++ frontend/src/routes/AdminGate.svelte
@@ -2,7 +2,7 @@
       import AdminGateForm from "../components/admin-gate/AdminGateForm.svelte";
 </script>
 
-<div class="min-h-screen w-full px-4 flex flex-col justify-center items-center bg-[#8a7143] text-[#fdfaf5] gap-8">
+<div class="min-h-screen w-full px-4 flex flex-col justify-center items-center text-[#fdfaf5] gap-8">
       <div class="flex flex-col items-center gap-2">
             <h1 class="text-4xl font-bold">Admin Gate</h1>
             <p class="opacity-80 text-center">Enter your credentials to access the admin system</p>
