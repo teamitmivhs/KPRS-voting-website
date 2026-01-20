c64ec7a144fb8bc783d7e36c498fccb8d017fd33
Iwansal64 <iwansal64@gmail.com>
2026-01-20 08:57:29 +0700

    [improve] Add scrolling ability for the votes table

--- frontend/src/components/admin-dashboard/VotesTable.svelte
+++ frontend/src/components/admin-dashboard/VotesTable.svelte
@@ -10,50 +10,54 @@
         <div class="flex flex-col p-4 space-y-4">
                 <h3 class="font-bold w-full text-center px-4 py-2 text-2xl">Kampus MM</h3>
                 <div class="border border-white flex flex-col rounded-2xl">
-                        <div class="grid grid-rows-12 overflow-y-auto *:grid *:grid-cols-[24px_1fr_1fr] *:place-items-center">
-                                <div class="font-semibold border-b border-white p-4">
+                        <div class="flex flex-col h-[750px] overflow-y-auto *:place-items-center">
+                                <div class="grid grid-cols-[24px_1fr_1fr] font-semibold border-b border-white p-4">
                                         <div>
                                                 No.
                                         </div>
                                         <div>
-                                                Voter
+                                                Pemilih
                                         </div>
                                         <div>
-                                                Candidate
+                                                Kandidat
                                         </div>
                                 </div>
-                                {#each $useVotesData.MM as voteData, index}
-                                <div class="px-4 py-2 text-sm font-thin">
-                                        <div class="w-full text-center">{index+1}</div>
-                                        <div>{voteData.voter_name}</div>
-                                        <div>{voteData.candidate_name}</div>
+                                <div class="grid-flow-row max-h-[800px] overflow-y-auto *:w-full *:h-16 *:items-center *:grid *:grid-cols-[24px_1fr_1fr]">
+                                        {#each $useVotesData.MM as voteData, index}
+                                        <div class="px-4 py-2 text-xs font-thin">
+                                                <div class="w-full text-center">{index+1}</div>
+                                                <div>{voteData.voter_name}</div>
+                                                <div>{voteData.candidate_name}</div>
+                                        </div>
+                                        {/each}
                                 </div>
-                                {/each}
                         </div>
                 </div>
         </div>
         <div class="flex flex-col p-4 space-y-4">
                 <h3 class="font-bold w-full text-center px-4 py-2 text-2xl">Kampus PD</h3>
                 <div class="border border-white flex flex-col rounded-2xl">
-                        <div class="grid grid-rows-12 *:grid *:grid-cols-[24px_1fr_1fr] *:place-items-center">
-                                <div class="font-semibold border-b border-white p-4">
+                        <div class="flex flex-col h-[750px] overflow-y-auto *:place-items-center">
+                                <div class="grid grid-cols-[24px_1fr_1fr] font-semibold border-b border-white p-4">
                                         <div>
                                                 No.
                                         </div>
                                         <div>
-                                                Voter
+                                                Pemilih
                                         </div>
                                         <div>
-                                                Candidate
+                                                Kandidat
                                         </div>
                                 </div>
-                                {#each $useVotesData.PD as voteData, index}
-                                <div class="px-4 py-2 text-sm font-thin">
-                                        <div class="w-full text-center">{index+1}</div>
-                                        <div>{voteData.voter_name}</div>
-                                        <div>{voteData.candidate_name}</div>
+                                <div class="grid-flow-row max-h-[800px] overflow-y-auto *:w-full *:h-16 *:items-center *:grid *:grid-cols-[24px_1fr_1fr]">
+                                        {#each $useVotesData.PD as voteData, index}
+                                        <div class="px-4 py-2 text-xs font-thin">
+                                                <div class="w-full text-center">{index+1}</div>
+                                                <div>{voteData.voter_name}</div>
+                                                <div>{voteData.candidate_name}</div>
+                                        </div>
+                                        {/each}
                                 </div>
-                                {/each}
                         </div>
                 </div>
         </div>
